169cc06493fe0610296d23019dbe2ff3ac9c9fda
Adam DeHaven <adam.dehaven@konghq.com>
2025-10-22 13:52:17 -0400

    fix: ignore out

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,12 @@ import eslintKongUiConfig from '@kong/eslint-config-kong-ui'
 export default [
   ...eslintKongUiConfig,
   {
+    ignores: [
+      'dist/**/*',
+      'out/**/*',
+    ],
+  },
+  {
     files: ['src/webview/**/*.js'],
     languageOptions: {
       globals: {
