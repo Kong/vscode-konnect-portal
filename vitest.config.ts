79e492ddfbd18cf11b4c72598efa99c29efb52d9
Adam DeHaven <adam.dehaven@konghq.com>
2025-11-02 10:37:44 -0500

    test: add test coverage
    Add vitest.

---
+++ vitest.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig, mergeConfig } from 'vitest/config'
+import viteConfig from './vite.config'
+
+export default mergeConfig(viteConfig, defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    include: ['**/*.spec.ts'],
+    exclude: [
+      './dist/**',
+      './out/**',
+      'node_modules',
+    ],
+    deps: {
+      optimizer: {
+        web: {
+          // https://github.com/vitest-dev/vitest/issues/4074
+          exclude: ['vue'],
+        },
+      },
+    },
+  },
+}))
